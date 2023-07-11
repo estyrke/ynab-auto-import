73da2703d1b9c87029501363bdbd68c9be543c83
Emil Styrke <emil.styrke@axis.com>
2023-07-11 20:08:12 +0000

    chore: upgrade deps

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,12 +1,10 @@
-import { Link as ChakraLink, LinkProps as ChakraLinkProps } from '@chakra-ui/react';
+import { Link as ChakraLink, LinkProps as ChakraLinkProps, chakra } from '@chakra-ui/react';
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
 
-// Only pick href from NextLinkProps. Add more if needed.
-export type LinkProps = Pick<NextLinkProps, "href"> & ChakraLinkProps;
-
-export const Link = ({ children, href, ...props }: LinkProps) =>
-  <NextLink href={href} passHref>
-    <ChakraLink {...props}>{children}</ChakraLink>
-  </NextLink>;
+// wrap the NextLink with Chakra UI's factory function
+const Link = chakra<typeof NextLink, NextLinkProps>(NextLink, {
+  // ensure that you're forwarding all of the required props for your case
+  shouldForwardProp: (prop) => ['href', 'target', 'children'].includes(prop),
+})
 
 export default Link;
